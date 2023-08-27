d4d8d415f1d322fdfa810c8ff02b46c839cd2adb
Glumboi <58010838+Glumboi@users.noreply.github.com>
2023-08-27 17:45:08 +0200

    Add more features, UI improvements

--- src/DinoScapeMelonMod/Main.cs
+++ src/DinoScapeMelonMod/Main.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Threading;
+using HarmonyLib;
 using Il2Cpp;
 using MelonLoader;
 using UnityEngine;
@@ -9,7 +11,10 @@ namespace DinoScapeMelonMod
     public static class BuildInfo
     {
         public const string Name = "DinoScapeMelonMod"; // Name of the Mod.  (MUST BE SET)
-        public const string Description = "Mod that enables some cheats for you"; // Description for the Mod.  (Set as null if none)
+
+        public const string
+            Description = "Mod that enables some cheats for you"; // Description for the Mod.  (Set as null if none)
+
         public const string Author = "Glumboi"; // Author of the Mod.  (MUST BE SET)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
         public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
@@ -18,14 +23,13 @@ namespace DinoScapeMelonMod
 
     public class DinoScapeMelonMod : MelonMod
     {
-        private bool menuEnabled;
-        private Rect windowRect = new Rect(20, 20, 350, 620);
+        private static bool menuEnabled;
 
         public override void OnUpdate() // Runs once per frame.
         {
             if (Input.GetKeyDown(KeyCode.F2))
             {
-                menuEnabled = !menuEnabled;
+                ToggleMenu();
             }
 
             if (Input.GetKeyDown(KeyCode.F3))
@@ -41,26 +45,33 @@ namespace DinoScapeMelonMod
                 UI.DoMyWindow();
             }
         }
+        
+        public static void ToggleMenu()
+        { 
+            menuEnabled = !menuEnabled;
+        }
     }
-    
+
     [HarmonyPatch(typeof(ZoneMove))]
     internal static class ZonePatches
     {
+        public static bool zoneDontMove = false;
+        
         [HarmonyPatch(typeof(ZoneMove), "Start")]
         [HarmonyPrefix]
         static void Prefix_Start(ZoneMove __instance)
         {
-            MelonLogger.Msg("Disabling zone movement...");
-            __instance.DontMove = true;
+            __instance.DontMove = zoneDontMove;
         }
     }
 
-        
+
     [HarmonyPatch(typeof(GetDisplayname))]
     internal static class GetDisplaynamePatches
     {
         public static GetDisplayname displayNameInstance;
-        
+        public static string newUsername = string.Empty;
+
         [HarmonyPatch(typeof(GetDisplayname), "Start")]
         [HarmonyPrefix]
         static void Prefix_Start(GetDisplayname __instance)
@@ -68,7 +79,7 @@ namespace DinoScapeMelonMod
             displayNameInstance = __instance;
         }
     }
-    
+
     //works sometimes
     [HarmonyPatch(typeof(CheckForBan))]
     internal static class BanPatches
@@ -81,7 +92,7 @@ namespace DinoScapeMelonMod
                 $"[From Start method of CheckForBan] User banned original : {__instance.banned} User banned new: {false}");
             __instance.banned = false;
         }
-        
+
         [HarmonyPatch(typeof(CheckForBan), "Awake")]
         [HarmonyPostfix]
         static void Postfix_Awake(CheckForBan __instance)
@@ -98,40 +109,129 @@ namespace DinoScapeMelonMod
         {
             MelonLogger.Msg(
                 $"[From CheckBan method of CheckForBan] User banned original : {__instance.banned} User banned new: {false}");
+
             __instance.banned = false;
         }
     }
-    
+
     [HarmonyPatch(typeof(Emerald))]
     internal static class EmeraldPatches
     {
+        public static bool patchRewards = false;
+        public static int minReward = 8000;
+        public static int maxReward = 19356;
+        
         [HarmonyPatch(typeof(Emerald), "Start")]
         [HarmonyPostfix]
         static void Postfix_Start(Emerald __instance)
         {
-            var rnd = new Random();
-            int addVal = rnd.Next(8000, 19356);
-            MelonLogger.Msg($"Patching one coin's value from: {__instance.coinValue} to: {__instance.coinValue += addVal}");
-            __instance.coinValue += addVal;
+            if (patchRewards)
+            {
+                var rnd = new Random();
+                int newVal = rnd.Next(minReward, maxReward);
+                MelonLogger.Msg(
+                    $"Patching one coin's value from: {__instance.coinValue} to: {__instance.coinValue += newVal}");
+                __instance.coinValue = newVal;
+            }
         }
-    }    
-    
+    }
+
     [HarmonyPatch(typeof(PlayerMovement))]
     internal static class PlayerMovementPatches
     {
         public static bool boost = false;
-        
+        public static float boostMp = 2.1f;
+
         [HarmonyPatch(typeof(PlayerMovement), "Update")]
         [HarmonyPrefix]
         static void Prefix_Update(PlayerMovement __instance)
         {
             if (boost)
             {
-                __instance.runSpeed = __instance.runSpeedBase * 2;
+                __instance.runSpeed = __instance.runSpeedBase * boostMp;
             }
             else
             {
                 __instance.runSpeed = __instance.runSpeedBase;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(AntiCheat))]
+    internal static class AntiCheatPatches
+    {
+        [HarmonyPatch(typeof(AntiCheat), "isComputer")]
+        [HarmonyPrefix]
+        static bool Prefix_isComputer(AntiCheat __instance)
+        {
+            MelonLogger.Msg("Spoofing anticheat...");
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(IncubatorManager))]
+    internal static class IncubatorManagerPatches
+    {
+        public static IncubatorManager incuInstance;
+        
+        [HarmonyPatch(typeof(IncubatorManager), "Awake")]
+        [HarmonyPostfix]
+        static void Postfix_Awake(IncubatorManager __instance)
+        {
+            incuInstance = __instance;
+        }
+
+        public static void SetEggTypeGoalMeters(int m)
+        {
+            var egg = incuInstance.selectedEgg;
+            if (incuInstance != null)
+            {
+                if (egg != null)
+                {
+                    incuInstance.selectedEgg.goalMeters = m;
+                    MelonLogger.Msg("Trying to set an eggs goal...");
+                    return;
+                }
+                MelonLogger.Msg("No selected egg found!");
+                return;                
+            }
+            
+            MelonLogger.Msg("No incubator instance found!");
+        }
+        
+        public static void HatchSelectedEgg()
+        {
+            var egg = incuInstance.selectedEgg;
+            if (incuInstance != null)
+            {
+                if (egg != null)
+                {
+                    incuInstance.selectedEgg.Hatch();
+                    MelonLogger.Msg("Trying to hatch an egg...");
+                    return;
+                }
+                MelonLogger.Msg("No selected egg found!");
+                return;                
+            }
+            
+            MelonLogger.Msg("No incubator instance found!");
+        }
+    }
+    
+    [HarmonyPatch(typeof(DinoEgg))]
+    internal static class DinoEggPatches
+    {
+        public static bool cheatAmber = false;
+        public static int cheatAmberAmount = 999;
+        
+        [HarmonyPatch(typeof(DinoEgg), "GrantContent", new Type[] {typeof(DinoEgg.EggContent)})]
+        [HarmonyPrefix]
+        static void Prefix_GrantContent(DinoEgg.EggContent content)
+        {
+            if (cheatAmber)
+            {
+                content.amount = cheatAmberAmount;
+                content.contenttype = DinoEgg.EggContent.ContentTypes.Currency;
             }
         }
     }
--- src/DinoScapeMelonMod/UI.cs
+++ src/DinoScapeMelonMod/UI.cs
@@ -15,15 +15,28 @@ namespace DinoScapeMelonMod
     {
         public static void DoMyWindow()
         {
-            GUILayout.BeginVertical(GUI.skin.box, GUILayout.Width(300));
-            GUI.backgroundColor = Color.red;
+            GUILayout.BeginVertical(GUI.skin.window, new GUILayoutOption[] { GUILayout.Width(300)});
+            GUI.backgroundColor = new Color32(255, 0, 147, 255);
+            GUI.contentColor = new Color32(0, 198, 255, 255);
             
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Close", new GUILayoutOption[0]))
+            {
+                DinoScapeMelonMod.ToggleMenu();
+            }
+
+            GUILayout.Space(6);
+            GUILayout.Label("Dinoscape Cheats", new GUILayoutOption[0]);
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(6);
+
             if (GUILayout.Button("Fill all slots with eggs", new GUILayoutOption[0]))
             {
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < 6; i++)
                 {
                     GameInstances.cM.GiveDinoEgg();
-                    
+
                     MelonLogger.Msg("Gave 1 egg from the CheatManager!");
                 }
             }
@@ -33,30 +46,64 @@ namespace DinoScapeMelonMod
                 GameInstances.cM.GiveDinoBrachio();
                 GameInstances.cM.GiveDinoEuplo();
                 GameInstances.cM.GiveDinoTrizera();
-                
+
                 MelonLogger.Msg("Gave all available dino's from the CheatManager!");
-            }            
-            
+            }
+
+            if (GUILayout.Button("Hatch selected egg", new GUILayoutOption[0]))
+            {
+                IncubatorManagerPatches.HatchSelectedEgg();
+            }
+
+            if (GUILayout.Button("Patch all egg meters to 1M", new GUILayoutOption[0]))
+            {
+                IncubatorManagerPatches.SetEggTypeGoalMeters(1);
+            }
+
+            GUILayout.Space(8);
+            GUILayout.Label("Collectable Emerald rewards");
+            GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical();
+            GUILayout.Label("Minimum reward");
+            EmeraldPatches.minReward = Int32.Parse(GUILayout.TextField(EmeraldPatches.minReward.ToString()));
+            GUILayout.EndVertical();
+            GUILayout.BeginVertical();
+            GUILayout.Label("Maximum reward");
+            EmeraldPatches.maxReward = Int32.Parse(GUILayout.TextField(EmeraldPatches.maxReward.ToString()));
+            GUILayout.EndVertical();
+            GUILayout.EndHorizontal();
+            EmeraldPatches.patchRewards = GUILayout.Toggle(EmeraldPatches.patchRewards, "Toggle patched rewards");
+
+
+            GUILayout.Space(8);
+            GUILayout.Label("New username");
+            GetDisplaynamePatches.newUsername = GUILayout.TextField(GetDisplaynamePatches.newUsername);
             if (GUILayout.Button("Change username", new GUILayoutOption[0]))
             {
-                string newUserName;
-                if (!File.Exists("./NewUserName.txt"))
-                    File.Create("./NewUserName.txt").Close();
-
-                newUserName = File.ReadAllText("./NewUserName.txt");
-                if (!string.IsNullOrWhiteSpace(newUserName))
+                if (!string.IsNullOrWhiteSpace(GetDisplaynamePatches.newUsername))
                 {
-                    GetDisplaynamePatches.displayNameInstance.SaveName(newUserName);
+                    GetDisplaynamePatches.displayNameInstance.SaveName(GetDisplaynamePatches.newUsername);
+                    MelonLogger.Msg($"Trying to change username to: {GetDisplaynamePatches.newUsername}");
                 }
             }
-            
+
             GUILayout.Space(8);
-            
-            if (GUILayout.Button("Toggle speed boost", new GUILayoutOption[0]))
-            {
-                PlayerMovementPatches.boost = !PlayerMovementPatches.boost;
-            }
-            GUILayout.Label($"Super speed toggle state: {PlayerMovementPatches.boost}");
+            GUILayout.Label("Speedhack multiplier");
+            PlayerMovementPatches.boostMp = float.Parse(GUILayout.TextField(PlayerMovementPatches.boostMp.ToString("0.0")));
+            PlayerMovementPatches.boost = GUILayout.Toggle(PlayerMovementPatches.boost, "Toggle Speedhack");
+
+            GUILayout.Space(8);
+            GUILayout.Label("Amount of Amber/Emrald to grand on egg hatch");
+            DinoEggPatches.cheatAmberAmount =
+                Int32.Parse(GUILayout.TextField(DinoEggPatches.cheatAmberAmount.ToString()));
+            DinoEggPatches.cheatAmber = GUILayout.Toggle(DinoEggPatches.cheatAmber, "Toggle Amber/Emrald hatch cheat");
+
+            GUILayout.Space(8);
+            GUILayout.Label("Other settings");
+            GUILayout.Space(4);
+            ZonePatches.zoneDontMove =
+                GUILayout.Toggle(ZonePatches.zoneDontMove, "Stop zone movement (needs a level reload)");
+
             GUILayout.EndVertical();
         }
     }
